a030321233b5258faea6c04b3d8cf2cd496c1589
Ben Lee <ben_lee@post.harvard.edu>
2022-03-15 22:28:54 -0700

    setup.py imports requirements-noversion

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
-import setuptools; setuptools.setup()
+import setuptools
+
+with open('requirements-noversion.txt') as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name='plenoxels',
+    version='0.1.0',
+    install_requires=requirements
+)
+
